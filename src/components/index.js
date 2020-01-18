19e749a30ea2a0131645bcc8b1fdca28581c3c54
Sami Beig <Sami.Beig45@myhunter.cuny.edu>
2020-01-18 15:16:58 -0500

    fixed compiliing erros revolving index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as NavBar } from "./utilities/NavBar";
 export { default as Home } from "./Home/HomeContainer";
-export { default as Home } from "./views/Home";
 export { default as GameView } from "./views/GameView";
 export { default as TrailerModal } from "./utilities/TrailerModal";
