50902c4a6a6e28c004fb9caa4f7d7228e80f4e4c
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2020-01-18 15:27:37 -0500

    styling changes

--- src/components/views/GameView.js
+++ src/components/views/GameView.js
@@ -8,12 +8,12 @@ const GameView = () => {
     <div className="main-container">
       <div className="game-header">
         <img src="https://static-cdn.jtvnw.net/ttv-boxart/Fortnite-144x192.jpg" style={{float:'left', marginTop:'30px', marginLeft:'40px'}}/>
-        <div style={{float:'left', marginLeft:'40px', textAlign:'left', color:'white', width:'30%'}}>
+        <div style={{float:'left', marginTop:'-40px', marginLeft:'40px', textAlign:'left', color:'white', width:'30%'}}>
           <h1>Fortnite</h1>
           <h2> 74.8K Viewers . 51.7M Followers </h2>
           <p style={{color:'white', fontSize:'14px', marginLeft:0}}> Fortnite is a sandbox-survival third-person shooter from the studio behind the Unreal Tournament and Gears of War series. </p>
         </div>
-        <div style={{float:'left', marginLeft:'100px', marginTop: '80px', textAlign:'left', width:'15%'}}>
+        <div style={{float:'left', marginTop:'40px', marginLeft:'100px', textAlign:'left', width:'15%'}}>
           <h2>Rating: 4.1/5</h2>
           <button className="steam-button"><a href="https://www.epicgames.com/fortnite"> Download Game </a> </button>
           <TrailerModal />
