05c5248d89fdef0f9e5b56d7af6335821503a03f
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2020-01-18 02:03:03 -0500

    added game page view plus styling

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as NavBar } from "./utilities/NavBar";
-export { default as Home } from "./Home";
+export { default as Home } from "./views/Home";
+export { default as GameView } from "./views/GameView";
---
+++ src/components/views/GameView.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./GameView.css";
+
+const GameView = () => {
+  return (
+    <div className="main-container">
+      <div className="game-header">
+        <img src="https://static-cdn.jtvnw.net/ttv-boxart/Fortnite-144x192.jpg" style={{float:'left', marginTop:'30px', marginLeft:'40px'}}/>
+        <div style={{float:'left', marginLeft:'40px', textAlign:'left',}}>
+          <h1>Fortnite</h1>
+          <h2> 74.8K Viewers . 51.7M Followers </h2>
+          <p> Fortnite is a sandbox-survival third-person shooter from the <br/> studio behind the Unreal Tournament and Gears of War series. </p>
+        </div>
+        <div style={{float:'left', marginLeft:'100px', marginTop: '80px', textAlign:'left',}}>
+          <h2>Rating: 4.1/5</h2>
+          <button className="steam-button"> Buy on Steam </button>
+        </div>
+      </div>
+      <h1> Twitch Streams </h1>
+      <div>
+        <img className="stream-box" src="https://i.ytimg.com/vi/Kavmf3RYEjI/maxresdefault.jpg"/>
+        <img className="stream-box-blue" src="https://i.ytimg.com/vi/Kavmf3RYEjI/maxresdefault.jpg"/>
+        <img className="stream-box" src="https://i.ytimg.com/vi/Kavmf3RYEjI/maxresdefault.jpg"/>
+        <img className="stream-box-blue" src="https://i.ytimg.com/vi/Kavmf3RYEjI/maxresdefault.jpg"/>
+      </div>
+    </div>
+  );
+};
+
+export default GameView;
