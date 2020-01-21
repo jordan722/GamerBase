9e546f7b6f46b05276906b457cfb9afa5cd32cd3
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2020-01-21 14:55:24 -0500

    redux bug fix

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -3,7 +3,7 @@ export const ADD_USER = "ADD_USER";
 export const GET_USERS = "GET_USERS";
 export const EDIT_USER = "EDIT_USER";
 export const REMOVE_USER = "REMOVE_USER";
-export const GET_USERS = "GET_USERS";
+export const GET_USER = "GET_USER";
 
 
 // Game actions
