b18131c1a3f9ff130f97ebd347535d72e9b34218
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-01-24 01:22:54 -0500

    added names to replies

--- src/components/Forum/ThreadView.js
+++ src/components/Forum/ThreadView.js
@@ -4,9 +4,11 @@ import "./Forum.css";
 const ThreadView = props => {
 	const { reply } = props;
 
+	console.log("view", reply);
+
 	return (
 		<div id="reply">
-			<p className="reply-box">{reply.userId}</p>
+			<p className="reply-box">{reply.userName}</p>
 			<p className="reply-box">{reply.title}</p>
 		</div>
 	);
